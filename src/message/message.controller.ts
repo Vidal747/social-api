b0bda20a690215b3bd33075173843c7cc97cc8e7
Vidal_747 <emmanuelvidal747@hotmail.com>
2024-04-13 08:30:36 -0500

    CRUD completo

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('message')
 export class MessageController {
     @Get()
-    getMessages(): string{
-        return 'hello!!!!'
+    getMessages(@Query() filterQuery): string{
+        const {searchTerm, orderBy} = filterQuery
+        return `Tosdos los  ${searchTerm} Mensajes fueron ordenados por ${orderBy}`
     }
     
     @Get(':id')
@@ -13,7 +14,20 @@ export class MessageController {
     }
 
     @Post()
-    createMessage(@Body('message')message: String){
+    @HttpCode(HttpStatus.NO_CONTENT)
+    createMessage(@Body('message')message: String): string{
         return `Tu mensaje es: ${message}!!`;
     }
+
+    @Patch(':id')
+    updateMessage(@Param('id')id: string,@Body() message): string{
+        return `el Mensaje ${id} ha sido actualizado`
+    }
+    @Delete(':id')
+   deleteMessage(@Param('id')id: string){
+        return `el Mensaje ${id} ha sido eliminado`
+    }
+ 
 }
+
+
